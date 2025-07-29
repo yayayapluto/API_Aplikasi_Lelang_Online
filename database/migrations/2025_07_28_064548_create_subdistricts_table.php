dbd756ff5c9f96551aff586ddbd8ff30b2abd193
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-07-29 12:10:07 +0700

    fix: subdistrict migration

--- database/migrations/2025_07_28_064548_create_subdistricts_table.php
+++ database/migrations/2025_07_28_064548_create_subdistricts_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('subdistricts', function (Blueprint $table) {
             $table->id();
             $table->string("nama")->unique();
-            $table->foreignId("city_id")->constrained("cities")->cascadeOnDelete();
+            $table->integer("fullCode")->unique();
+            $table->integer("code")->unique()->nullable();
             $table->timestamps();
         });
     }

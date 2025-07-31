f50e53729bcd365f5b3f22378191696dd0a649cd
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-07-31 07:59:41 +0700

    chore: user and seller migration

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Seller extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<\Database\Factories\SellerFactory> */
     use HasFactory;
 }
